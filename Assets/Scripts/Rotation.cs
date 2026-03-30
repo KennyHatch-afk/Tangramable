8af374f4387dd29c20fc7b072f4d44632f12bbeb
KennyHatch-afk <kah8090@g.rit.edu>
2026-03-30 19:50:25 -0400

    Rotation is complete
    The Rotation circle can be attached to any object to give it the ability to be rotated.

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+
+public class Rotation : MonoBehaviour
+{
+    public Camera mainCam;
+    public Vector3 unitCircleVector;
+    public Vector3 unitMouseVector;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 worldMousePosition = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        unitCircleVector = transform.position - transform.parent.position;
+        unitCircleVector.z = 0;
+        unitMouseVector = worldMousePosition - transform.parent.position;
+        unitMouseVector.z = 0;
+
+        Vector3 cross = Vector3.Cross(unitMouseVector, unitCircleVector);
+        float temp = Vector3.SignedAngle(unitCircleVector, unitMouseVector, Vector3.forward);
+
+        //Debug.DrawLine(transform.parent.position, transform.parent.position + unitCircleVector, Color.green);
+        //Debug.DrawLine(transform.parent.position, transform.parent.position + unitMouseVector, Color.red);
+        //Debug.Log(temp);
+
+        transform.parent.transform.Rotate(0, 0, temp);
+    }
+}
