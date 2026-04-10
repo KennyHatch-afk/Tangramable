c6cd9a2f8325f8be662cef906456d5ea99c7441f
Gmmolly05 <gmm9098@rit.edu>
2026-04-09 20:31:37 -0400

    Added dynamic spawning of pieces and silhouettes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,7 @@ using UnityEngine.SocialPlatforms.Impl;
 public class GameManager : MonoBehaviour
 {
     public LevelData currentLevel;
-    private int counter;
+    private int counter = 1;
 
     public List<Piece> currentPieces;
 
@@ -17,10 +17,26 @@ public class GameManager : MonoBehaviour
 
     public GameObject levelText;
 
+    private Dictionary<string, GameObject> levelPrefabs = new Dictionary<string, GameObject>();
+
+    public List<GameObject> prefabs = new List<GameObject>();
+
+    private Dictionary<string, GameObject> levelSihlouettes = new Dictionary<string, GameObject>();
+
+    public List<GameObject> sihlouettes = new List<GameObject>();
+
     void Start()
     {
-        counter++;
-        currentPieces = new List<Piece>(FindObjectsByType<Piece>(FindObjectsSortMode.None));
+
+        foreach (GameObject p in prefabs)
+        {
+            levelPrefabs.Add(p.name, p);
+        }
+
+        foreach (GameObject s in sihlouettes)
+        {
+            levelSihlouettes.Add(s.name, s);
+        }
         LoadLevel("level_" + counter);
         levelText.GetComponent<TextMeshProUGUI>().text = "Level 1: " + currentLevel.levelName;
     }
@@ -28,7 +44,7 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         gameWon = CheckForWin();
-        if(gameWon)
+        if (gameWon)
         {
             counter++;
             LoadLevel("level_" + counter);
@@ -52,18 +68,41 @@ public class GameManager : MonoBehaviour
 
     public void LoadLevel(string levelName)
     {
+        Debug.Log("called load level");
         currentLevel = Resources.Load<LevelData>("Levels/" + levelName);
         gameWon = false;
+        SpawnPieces();
+        FindPieces();
         SetSolutions();
+        SpawnSihlouette();
         //Debug.Log(currentLevel);
+    }
+
+    public void FindPieces()
+    {
+        currentPieces = new List<Piece>(FindObjectsByType<Piece>(FindObjectsSortMode.None));
+    }
+
+    public void SpawnPieces()
+    {
+        if (counter > 1)
+            Destroy(levelPrefabs["Level_" + (counter - 1)]);
+        Instantiate(levelPrefabs["Level_" + counter]);
     }
 
     public void SetSolutions()
     {
-        foreach(Piece p in currentPieces)
+        foreach (Piece p in currentPieces)
         {
             p.SetTarget(currentLevel.positionSolution[p.id], currentLevel.rotationSolution[p.id]);
         }
+    }
+
+    public void SpawnSihlouette()
+    {
+        if (counter > 1)
+            Destroy(levelSihlouettes["level_" + (counter - 1) + "_Soln"]);
+        Instantiate(levelSihlouettes["level_" + counter + "_Soln"]);
     }
 
 }
