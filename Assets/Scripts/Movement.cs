b891859797c231c75805f2108b723f8c6cbc15a9
KennyHatch-afk <kah8090@g.rit.edu>
2026-04-22 17:47:27 -0400

    Changed start screen background color and added QE rotation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,6 +12,10 @@ public class Movement : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDrag
     public Vector3 circleDistance;
     public PieceDistance pieceTracker;
 
+    public InputAction rotateLeft;
+    public InputAction rotateRight;
+    public bool isSelected = false;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,6 +27,9 @@ public class Movement : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDrag
         circleDistance = transform.position - transform.parent.position;
 
         circleDistance.z = transform.position.z;
+
+        rotateLeft.Enable();
+        rotateRight.Enable();
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -37,7 +44,6 @@ public class Movement : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDrag
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("Begin Drag");
         Vector3 worldMousePosition = mainCam.ScreenToWorldPoint(new Vector3(Mouse.current.position.x.ReadValue(), Mouse.current.position.y.ReadValue(), 1));
 
         worldMousePosition.z = transform.position.z;
@@ -48,10 +54,28 @@ public class Movement : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDrag
         transform.parent.position = worldMousePosition - circleDistance;
 
         transform.parent.Translate(0, 0, -1f);
+
+        isSelected = true;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         transform.parent.position = new Vector3(transform.parent.position.x, transform.parent.position.y, 0f);
+        isSelected = false;
+    }
+
+    public void Update()
+    {
+        if (isSelected)
+        {
+            if (rotateLeft.IsPressed() == true)
+            {
+                transform.parent.transform.Rotate(0, 0, -3);
+            }
+            else if (rotateRight.IsPressed() == true)
+            {
+                transform.parent.transform.Rotate(0, 0, 3);
+            }
+        }
     }
 }
