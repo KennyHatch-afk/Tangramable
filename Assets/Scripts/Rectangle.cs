fa68e85cb351014e09ae63ab831416c0f8234b3f
Gmmolly05 <gmm9098@rit.edu>
2026-04-21 20:13:21 -0400

    fixed angle check for rectangle

--- Assets/Scripts/Rectangle.cs
+++ Assets/Scripts/Rectangle.cs
@@ -6,16 +6,14 @@ public class Rectangle : Piece
     public override bool isCorrect()
     {
         float positionDifference = Vector2.Distance(transform.localPosition, targetPosition);
-        float angleDifference = Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.z, targetAngle));
 
-        // Adjust the angle tolerance based on the rotation of the rectangle
-        float adjustedAngleTolerance = angleTolerance;
-        if (Mathf.Abs(transform.eulerAngles.z - targetAngle) > 180f)
-        {
-            adjustedAngleTolerance = 360f - angleTolerance;
-        }
+        float angleToTarget = Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.z, targetAngle));
+        float angleToFlipped = Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.z, targetAngle + 180f));
 
-        return positionDifference <= positionTolerance && angleDifference <= adjustedAngleTolerance;
+        float angleDifference = Mathf.Min(angleToTarget, angleToFlipped);
+
+        return positionDifference <= positionTolerance &&
+               angleDifference <= angleTolerance;
     }
 
 }
